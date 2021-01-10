ab18363e3ce5a304921cf0b57aaee61dc82a2092
AlanGervin <56855066+AlanGervin@users.noreply.github.com>
2021-01-10 11:37:49 -0800

    Update index.js

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ function gifAlert(user, gif, audio, type) {
     //uncomment this if you want to play audio
     //audio.play();
     container.innerHTML = `
-      <h1 class="text-shadows">${user + generateTitle[type]}</h1>
+      //<h1 class="text-shadows">${user + generateTitle[type]}</h1>
       <img src="${gif}" />
     `;
     container.style.opacity = 1;
